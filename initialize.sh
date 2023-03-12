0e225b5d94ad75f4194e55a5700f97d0ca1b1f3c
Mikhail Hyde <hyde.mikhail@gmail.com>
2023-03-12 13:26:07 -0400

    Improved initialization

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/bash
 
-export PYTHONPATH="${PYTHONPATH}:lib/"
+# Get Current Diretory Info
+DIR=$(pwd)
+BASE="$(basename $DIR)"
+
+if [ "${BASE}" != "rust_dsp" ]
+then
+    echo "Stopping Script! Please run this script from the rust_dsp directory"
+    exit 1
+fi
+
+# Make Virtualenvironment
+mkdir venv
+cd venv
+virtualenv -p /usr/bin/python3.10 DSP
+source DSP/bin/activate
+cd ../
+pip install -r requirements.txt
+deactivate
+
+# Run Python Initialization Script
 python3 tools/initialize.py
--- tools/initialize.py
+++ tools/initialize.py
@@ -1,16 +1,6 @@
 import os
 
-class ConfigurationError(Exception):
-    pass
-
 def main():
-    
-    if not os.path.exists("venv"):
-        raise ConfigurationError("Please generate a DSP virtualenv in a venv/ directory within this project")
-        
-    if not os.path.exists("venv/lord/"):
-        raise ConfigurationError("You have not created a virtual environment for the project named \"DSP\"")
-    
     # Create all required directories
     root = "data"
     dirs = ["graph", "csv", "tmp"]
